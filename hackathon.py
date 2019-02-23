91be6de2563d35424db538932292a3a162300c15
gtaylo27 <35612175+gtaylo27@users.noreply.github.com>
2019-02-22 23:04:37 -0500

    relapse count

--- hackathon.py
+++ hackathon.py
@@ -1,5 +1,14 @@
 import numpy as np
+import pandas as pd
 
-data = np.genfromtxt(open("services/client.csv", "r"), delimiter=",", skip_header=1)
+#data = np.genfromtxt(open("services/entry_exit.csv", "r"), delimiter=",", skip_header=1)
 
-print(data.shape)
+df = pd.read_csv("entry_exit/entry_exit.csv")
+client_id = df.client_id
+
+print(client_id.shape)
+
+appeared_ids
+for id in client_id:
+     is id in appeared_ids
+    
