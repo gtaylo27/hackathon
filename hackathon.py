2dd8a18b04b8c94e2f2d88c1148d93e01ea9b779
rlibberton <rlibberton@gmail.com>
2019-02-22 23:07:27 -0500

    racheal progress

--- hackathon.py
+++ hackathon.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-data = np.genfromtxt(open("services/client.csv", "r"), delimiter=",", skip_header=1)
+data = np.genfromtxt(open("entry_exit/entry_exit.csv", "r"), delimiter=",", skip_header=1)
 
 print(data.shape)
